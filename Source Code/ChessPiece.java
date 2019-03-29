c931ede0e58dfc8d41f414644a42f0679be27d36
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-03-29 19:48:35 +0000

    added knight subclass

--- Source Code/ChessPiece.java
+++ Source Code/ChessPiece.java
@@ -189,28 +189,6 @@ public class ChessPiece
 		}
 	}
 	
-	/*method determines whether a move is valid for a knight, no ChessPiece[][] parameter
-	 * because the knight jumps over pieces so we dont check if the path to its final 
-	 * position is clear*/
-	private boolean validKnightMove(int row, int column, ChessPiece[][] board)
-	{
-		/*Knights move in an L shaped pattern, the piece moves 2 squares on one axis and one square
-		 * on the other, resulting in 8 possible positions. If either the piece moves 2 squares 
-		 * vertically and one horizontally or 2 squares horizontally and one vertically, its a valid move.*/
-		if(Math.abs(this.column-column)==2 && Math.abs(this.row-row)==1)
-		{
-			return true;
-		}
-		else if(Math.abs(this.column-column)==1 && Math.abs(this.row-row)==2)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
 	/*method finds out whether there are any pieces between 2 positions that are either on
 	 *  the same row or column needed for queen and bishop moves.Public because it is also
 	 *   used in the method for the castling move in the ChessBoard class.*/
---
+++ Source Code/Knight.java
@@ -0,0 +1,38 @@
+
+public class Knight extends ChessPiece
+{
+    /*Constructor has parameters that specify the piece's type, team, and position*/
+    public Knight(PieceType type, ChessPlayer player, int row, int column)
+    {
+        super(type, player, row, column);
+    }
+    
+    /*method determines whether a move is valid for a knight, no ChessPiece[][] parameter
+     * because the knight jumps over pieces so we dont check if the path to its final 
+     * position is clear*/
+    @Override
+    public boolean validMove(int row, int column,ChessPiece[][] board)
+    {
+        /*Knights move in an L shaped pattern, the piece moves 2 squares on one axis and one square
+         * on the other, resulting in 8 possible positions. If either the piece moves 2 squares 
+         * vertically and one horizontally or 2 squares horizontally and one vertically, its a valid move.*/
+        if(Math.abs(this.column-column)==2 && Math.abs(this.row-row)==1)
+        {
+            return true;
+        }
+        else if(Math.abs(this.column-column)==1 && Math.abs(this.row-row)==2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+        return (super.toString() + "KN");
+    }
+}
